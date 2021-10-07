55d1025eabd12d9bbd094ed914753416f739a560
Yuki XD <pxnditxyr@gmail.com>
2021-10-07 18:56:23 -0400

    feat[js]: Added Remover Card with JavaScript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,9 @@
 import { addNewCard } from './addNewCard.js';
+import { removeCard } from './removeCard.js';
 
 const addButton = document.querySelector( "#add-card-button" );
+const removeButton = document.querySelector( "#remove-card-button" );
 const gallery = document.querySelector( "#gallery" );
 
 addButton.addEventListener( "click", () => addNewCard( gallery ), false );
+removeButton.addEventListener( "click", () => removeCard( gallery ), false );
